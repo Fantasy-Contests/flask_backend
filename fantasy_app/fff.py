fb97dc2467ac1691251ee04392d9de622de93d7f
Justin <41461549+justin-hsieh@users.noreply.github.com>
2022-11-15 11:50:30 -0800

    update fff

--- fantasy_app/fff.py
+++ fantasy_app/fff.py
@@ -3,11 +3,13 @@ from datetime import datetime
 from dotenv import load_dotenv
 from .espn_api_submodule.espn_api.football.league import League
 
+#load environment variables
 load_dotenv()
 LEAGUE_ID = os.getenv('LEAGUE_ID')
 ESPN_S2 = os.getenv('ESPN_S2')
 SWID = os.getenv('SWID')
 
+# retrieve football year
 def get_year():
     currentMonth = datetime.now().month
     if currentMonth > 7:
@@ -16,14 +18,10 @@ def get_year():
         currentYear = datetime.now().year - 1
     return currentYear
 
+# create league instance
 league = League(
     league_id=LEAGUE_ID,
     year=get_year(),
     espn_s2=ESPN_S2,
     swid=SWID
     )
-
-def current_week():
-    currentweek = league.current_week
-    return currentweek
-print(current_week())

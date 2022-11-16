bfb8b0a67b1e7036a5955e9683e7be3ee5b0b384
Justin <41461549+justin-hsieh@users.noreply.github.com>
2022-11-16 11:41:20 -0800

    test flask

--- fantasy_app/fff.py
+++ fantasy_app/fff.py
@@ -4,10 +4,13 @@ from dotenv import load_dotenv
 from .espn_api_submodule.espn_api.football.league import League
 
 #load environment variables
-load_dotenv()
-LEAGUE_ID = os.getenv('LEAGUE_ID')
-ESPN_S2 = os.getenv('ESPN_S2')
-SWID = os.getenv('SWID')
+#load_dotenv()
+#LEAGUE_ID = os.getenv('LEAGUE_ID')
+#ESPN_S2 = os.getenv('ESPN_S2')
+#SWID = os.getenv('SWID')
+LEAGUE_ID = 69192084
+ESPN_S2 = 'AECtBC8t2p%2F1HWtD9xwcZTJbKZnmIy2jPmG6JJMKDE7WjJ0YmGBLZG7i%2FOkb5aGb%2F%2BpKL%2Bxe33dXnl%2F6MDJwDbrD2thKx0SwXDB2wRJF%2F0oompDwq04%2BoR9qoX0777%2Bksnn2Hr55WelNUxLArj4Ea2XCCDCTO4S%2BgW147eUAqFSf28q93COtLYtnFk1uxKRkm2awp2ZxVycaBI2TMGeC1UNYUoP2wJBw%2F9s%2F2BPJKUC6hz8NtlXO2lSauKcY4cGxfSHfrfnOf0kgfJIJephWMeXYox9Hb7e7MRmd1TwLuL%2FP2w%3D%3D'
+SWID = '{832BF702-905D-40ED-ABF7-02905D90EDB1}'
 
 # retrieve football year
 def get_year():

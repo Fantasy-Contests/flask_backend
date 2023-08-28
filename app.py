f3ef438d8470f504d36340fd95707e1fc49ad8d7
Justin Hsieh <41461549+justin-hsieh@users.noreply.github.com>
2023-08-28 14:24:44 -0700

    Rearrange app

--- app.py
+++ app.py
@@ -3,23 +3,20 @@ from flask import Flask, jsonify, request
 ##from decouple import config
 from .fantasy_app.fff import current_week
 
-def create_app():
 
-    app = Flask(__name__)
-    #app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
 
-    # stop tracking modifications
-    #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
 
-    #DB.init_app(app)
+# stop tracking modifications
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#DB.init_app(app)
 
-    @app.route('/')
-    def hello1():
-        return 'Hello, World!!!!'
+@app.route('/')
+def hello1():
+    return 'Hello, World!!!!'
 
-    @app.route('/test_submodule', methods=['GET', 'POST'])
-    def submodule():
-        week = current_week()
-        return str(week)
-
-    return app
+@app.route('/test_submodule', methods=['GET', 'POST'])
+def submodule():
+    week = current_week()
+    return str(week)
